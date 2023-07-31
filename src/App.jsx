1cc56260a989a2b7f63a157a77e46f8bb4257f5d
Alvaro <alvaro.martin.crespo.00@gmail.com>
2023-07-31 13:01:44 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <div className="flex justify-center my-5">
+        <h1 className="font-bold">Chat in real time</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/supabase/client.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js';
+
+createClient(
+  'https://jfopwqikgmirylbuszkf.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Impmb3B3cWlrZ21pcnlsYnVzemtmIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTA3OTk2OTcsImV4cCI6MjAwNjM3NTY5N30.9TpVZ6AoWBhbA0qHbQs2zkAbQ2LO4eKXww7yezhd2q8'
+);
